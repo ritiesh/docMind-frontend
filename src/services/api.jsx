b0be4fa729d2843cf4d202b06d03531c6be2fb85
Ritiesh <ritiesh.it20@bitsathy.ac.in>
2026-03-25 22:22:36 +0530

    commited new url

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,4 +1,4 @@
-const API = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API = import.meta.env.VITE_API_URL || "https://aidocumentassistance-1.onrender.com/api";
 
 const api = {
   register: (data) =>
